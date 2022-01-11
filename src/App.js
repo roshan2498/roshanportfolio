a132277d8e5725a577b70e988768a31cb7c42f41
Roshan Kharke <roshan_bt2k16@dtu.ac.in>
2022-01-11 20:01:52 +0530

    added features

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './Home';
-import Resume from './Resume';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import Resume from "./Resume";
+import RickRoll from "./RickRoll";
 
 export default function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/resume" exact element={<Resume />} />
-            </Routes>
-        </BrowserRouter>
-
-    )
+      return (
+            <BrowserRouter>
+                  <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/resume" exact element={<Resume />} />
+                        <Route path="/onlyfans" exact element={<RickRoll />} />
+                  </Routes>
+            </BrowserRouter>
+      );
 }
---
+++ src/RickRoll.js
@@ -0,0 +1,13 @@
+import React, { useEffect } from "react";
+
+export default function Resume() {
+      useEffect(() => {
+            window.location.href =
+                  "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+      }, []);
+      return (
+            <div className="container my-5">
+                  <h3>Redirecting to OnlyFans..</h3>
+            </div>
+      );
+}
