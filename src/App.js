eaaf05e78076e0053616a38fb8c063d0d23e3027
Roshan Kharke <roshan_bt2k16@dtu.ac.in>
2022-01-11 18:41:13 +0530

    starting with 0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container my-4'>
+      <h1>👋 Roshan Kharke</h1>
+      <div className='container my-3 wrapper'>
+        <p>github: <a href="https://github.com/roshan2498">github.com/roshan2498</a></p>
+        <p>instagram: <a href="https://www.instagram.com/r0shan_cs">@r0shan_cs</a></p>
+        <p>linkedin: <a href="https://linkedin.com/in/roshankharke">linkedin.com/in/roshankharke/</a> </p>
+        <p>discord: <a href='https://discord.com/users/pix#2708'>pix#2708</a></p>
+        <p>steam: <a href='https://steamcommunity.com/id/pixcsgo'>@pixcsgo</a></p>
+        <p>gmail: <a href="mailto:roshan.kharke1111@gmail.com">roshan.kharke1111@gmail.com</a></p>
+        <p>telegram: <a href="https://t.me/r0shan_cs">@r0shan_cs</a></p>
+      </div>
     </div>
   );
 }
