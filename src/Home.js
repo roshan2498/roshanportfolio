dde59ba9072e8eb4135bc2d17494c47d6badc103
Roshan Kharke <roshan_bt2k16@dtu.ac.in>
2022-07-16 11:09:39 +0530

    added spotify playlist

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,66 @@
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className='container my-4'>
-      <h1>👋 Roshan Kharke</h1>
-      <div className='container my-3 wrapper'>
-        <p>github: <a href="https://github.com/roshan2498">github.com/roshan2498</a></p>
-        <p>instagram: <a href="https://www.instagram.com/r0shan_cs">@r0shan_cs</a></p>
-        <p>linkedin: <a href="https://linkedin.com/in/roshankharke">linkedin.com/in/roshankharke/</a> </p>
-        <p>discord: <a href='https://discord.com/users/pix#2708'>pix#2708</a></p>
-        <p>steam: <a href='https://steamcommunity.com/id/pixcsgo'>@pixcsgo</a></p>
-        <p>gmail: <a href="mailto:roshan.kharke1111@gmail.com">roshan.kharke1111@gmail.com</a></p>
-        <p>telegram: <a href="https://t.me/r0shan_cs">@r0shan_cs</a></p>
-      </div>
-    </div>
-  );
+      return (
+          <div className="container my-4">
+                  <h1>👋 Roshan Kharke</h1>
+                  <div className="container my-3 wrapper">
+                        <p>
+                              github:{" "}
+                              <a href="https://github.com/roshan2498">
+                                    github.com/roshan2498
+                              </a>
+                        </p>
+                        <p>
+                              instagram:{" "}
+                              <a href="https://www.instagram.com/r0shan_cs">
+                                    @r0shan_cs
+                              </a>
+                        </p>
+                        <p>
+                              linkedin:{" "}
+                              <a href="https://linkedin.com/in/roshankharke">
+                                    linkedin.com/in/roshankharke/
+                              </a>{" "}
+                        </p>
+                        <p>
+                              discord:{" "}
+                              <a href="https://discord.com/users/pix#2708">
+                                    pix#2708
+                              </a>
+                        </p>
+                        <p>
+                              steam:{" "}
+                              <a href="https://steamcommunity.com/id/pixcsgo">
+                                    @pixcsgo
+                              </a>
+                        </p>
+                        <p>
+                              gmail:{" "}
+                              <a href="mailto:roshan.kharke1111@gmail.com">
+                                    roshan.kharke1111@gmail.com
+                              </a>
+                        </p>
+                        <p>
+                              telegram:{" "}
+                              <a href="https://t.me/r0shan_cs">@r0shan_cs</a>
+                        </p>
+                  </div>
+                  <div className="spotify">
+                        <p>
+                            My Gym Playlist 👇 
+                        </p>
+                        <iframe
+                              src="https://open.spotify.com/embed/playlist/3LRGMv8m4JejxYGnMqHBox?utm_source=generator"
+                              width="30%"
+                              height="380"
+                              frameBorder="0"
+                              allowfullscreen=""
+                              allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+                        ></iframe>
+                  </div>
+            </div>
+      );
 }
 
 export default App;
